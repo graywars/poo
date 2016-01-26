116147c6788e3659a5f971ae8a5ddaba76efc688
graywars <alexis.ostalier@laposte.net>
2016-01-27 00:03:40 +0100

    Création de l'index
    Ajout de l'index des exercices.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*require_once("Ctab.php");
+$tab = [56, 18, 40, 2, 12, 60];
+$tri = new tab($tab);
+var_dump($tri->tri("decroissant"));*/
+require_once('Cpass.php');
+$pass = new pass("8");
+echo $pass->genere();
+?>
