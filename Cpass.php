61550d3a8791d79c3cd8cc226e974d463c3fc21f
graywars <alexis.ostalier@laposte.net>
2016-01-27 14:24:29 +0100

    Ajout methode get/set digit
    ajout des methode get et set pour le nombre de caractère et commentaires

--- Cpass.php
+++ Cpass.php
@@ -5,10 +5,30 @@ class pass
 	private $_carac = [["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], "", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]];
  	private $_pass = "";
 
- 	public function __construct($nb)
+ 	public function __construct()
  	{
- 		$this->_digit = $nb; // Définition du nombre de caractère du mot de passe
  	}
+
+ 	/*
+ 	*	Méthode set nombre caractère
+ 	*	Entrée:	int
+ 	*	Sortie:	void
+ 	*/
+ 	public function set_digit($digit)
+ 	{
+ 		$this->_digit = $digit;
+ 	}
+
+ 	/*
+ 	*	Méthode get nombre caractère
+ 	*	Entrée: void
+ 	*	Sortie: int
+ 	*/
+ 	public function get_digit()
+ 	{
+ 		return $this->_digit;
+ 	}
+
  	/*
  	*	Méthode de génération de mot de passe
  	*	Entrée:	void
