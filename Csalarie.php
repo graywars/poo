31b86176cc73223101dae7a6cb88d65c408f3069
graywars <alexis.ostalier@laposte.net>
2016-01-27 15:47:58 +0100

    Ajout méthode calcul salaire net
    Ajout d'une méthode de calcul de salaire net avec commentaires

--- Csalarie.php
+++ Csalarie.php
@@ -5,12 +5,27 @@ class salarie
 	private $_nom = "";
 	private $_prenom = "";
 	private $_salaire = 0.0;
-	private $_tauxcs = 0.0;
-	private $_tauxcp = 0.0;
+	private $_tauxcs = 10;
+	private $_tauxcp = 25;
 
-	function __construct()
+	function __construct($infos)
 	{
+		$this->_matricule = $infos[0];
+		$this->_nom = $infos[1];
+		$this->_prenom = $infos[2];
+		$this->_salaire = $infos[3];
+		$this->_tauxcs = $infos[4];
+		$this->_tauxcp = $infos[5];
+	}
 
+	/*
+	*	Méthode calcul salaire net
+	*	Entrée:	void
+	*	Sorite: float
+	*/
+	public function CalculerSalaireNet()
+	{
+		return $this->_salaire - ($this->_salaire * $this->_tauxcs);
 	}
 
 }
