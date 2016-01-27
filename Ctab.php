1e90d82891c66e02a97d812fdd5a13084e6840e3
graywars <alexis.ostalier@laposte.net>
2016-01-27 02:29:56 +0100

    Ajout class tab
    Ajout de la classe de trie de tableau

---
+++ Ctab.php
@@ -0,0 +1,38 @@
+<?php
+class tab
+{
+	private $_tab, $_trie = [];
+
+	public function __construct($tab)
+	{
+		$this->_tab = $tab;
+	}
+
+	public function tri($ordre)
+	{
+		foreach($this->_tab as $k => $v)
+		{
+			$nb_pos[$k] = 0;
+			foreach($this->_tab as $kn => $vn)
+			{
+				if($k != $kn && ($ordre == "croissant" && $v > $vn) || ($ordre == "decroissant" && $v < $vn))
+				{
+					$nb_pos[$k]++;
+				}
+			}
+		}
+		for($i = 0; $i != count($this->_tab); $i++)
+		{
+			for($j = 0; $j != count($this->_tab); $j++)
+			{
+				if($nb_pos[$j] == $i)
+				{
+					$this->_trie[$i] = $this->_tab[$j];
+					$j = count($this->_tab)-1;
+				}
+			}
+		}
+		return $this->_trie;
+	}
+}
+?>
