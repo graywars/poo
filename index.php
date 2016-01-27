796c5a2d0af25551ac9e6d5e9c5f757e643586da
graywars <alexis.ostalier@laposte.net>
2016-01-27 14:27:39 +0100

    Modification instance class
    Modification de l'instanciation des classes pass et tab

--- index.php
+++ index.php
@@ -7,13 +7,15 @@ if($_GET['class'] == 1)
 {
 	require_once("Ctab.php");
 	$tab = [56, 18, 40, 2, 12, 60];
-	$tri = new tab($tab);
+	$tri = new tab();
+	$tri->set_tab($tab);
 	var_dump($tri->tri("decroissant"));
 }
 elseif($_GET['class'] == 2)
 {
 	require_once('Cpass.php');
-	$pass = new pass("8");
+	$pass = new pass();
+	$pass->set_digit("8");
 	echo $pass->genere();
 }
 elseif($_GET['class'] == 3)
