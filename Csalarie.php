7eb9ef263750637547d09b433082066fe72b9b5b
graywars <alexis.ostalier@laposte.net>
2016-01-27 14:05:21 +0100

    Création class salarié
    Création de la classe salarié

---
+++ Csalarie.php
@@ -0,0 +1,16 @@
+<?php
+class salarie
+{
+	private $_matricule = 0;
+	private $_nom = "";
+	private $_prenom = "";
+	private $_salaire = 0.0;
+	private $_tauxcs = 0.0;
+	private $_tauxcp = 0.0;
+
+	function __construct()
+	{
+
+	}
+
+}
